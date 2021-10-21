e5bbbfcc3e711efe2d5d540d1bab78d35b4530db
Branislav Kadlec <cowmaster92@gmail.com>
2021-10-21 22:09:10 +0200

    Empty Project

---
+++ ScorchedEarthConsole.cpp
@@ -0,0 +1,12 @@
+﻿// ScorchedEarthConsole.cpp : Defines the entry point for the application.
+//
+
+#include "ScorchedEarthConsole.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
